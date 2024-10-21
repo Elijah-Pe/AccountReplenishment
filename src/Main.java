1bc52c020a6f5495a8d4577630a28a7fb04a8123
Ilya <ilia.petro2014@yandex.ru>
2024-10-21 15:49:35 +0400

    Add main programm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int onAccount = 50;
+        int replenishment = 999;
+        int bonus;
+
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+            onAccount = onAccount + replenishment + bonus;
+        } else {
+            bonus = 0;
+            onAccount = onAccount + replenishment + bonus;
+        }
+        System.out.println("Пополнение счета. Количество денег на счету: " +
+                onAccount);
+        System.out.println("Начисленные бонусы: " + bonus);
     }
 }
